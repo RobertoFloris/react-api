c749280245c646d83a42dd66d2c2e0b769521395
RobertoFloris <florisroberto99@gmail.com>
2025-01-22 16:24:27 +0100

    [add] card and bootstrap

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,14 @@
+import Cards from "./partials/Cards";
+
 const Posts = () => {
   return (
-    <div>Posts</div>
-  )
-}
+    <div className="container">
+      <h1 className="my-4 text-center">Gestione Post</h1>
+      <div className="row g-4">
+        <Cards />
+      </div>
+    </div>
+  );
+};
 
-export default Posts
+export default Posts;
