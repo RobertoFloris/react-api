afa7c330e2e62db59bf5322614e57c58422ca70c
RobertoFloris <florisroberto99@gmail.com>
2025-01-22 16:45:27 +0100

    [add] axios to fetchPosts in posts.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,22 @@
 import Cards from "./partials/Cards";
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 const Posts = () => {
+
+  const apiUrl = "http://localhost:3000"
+
+  const fetchPosts = () => {
+    axios.get(`${apiUrl}/posts`)
+      .then(res => {
+        console.log(res.data);
+      })
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <div className="container">
       <h1 className="my-4 text-center">Gestione Post</h1>
